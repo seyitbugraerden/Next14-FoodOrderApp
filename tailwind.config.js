558cc5001a47673e4379d10bb75c1eb8a727ef25
seyitbugraerden <seyitbugraerden@gmail.com>
2024-04-27 02:47:25 +0300

    File Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+     colors:{
+      primary : '#ffbe33',
+      secondary : '#222831'
+     },
+     fontFamily:{
+      dancing : ["Dancing Script", "cursive"]
+     }
     },
   },
   plugins: [],
