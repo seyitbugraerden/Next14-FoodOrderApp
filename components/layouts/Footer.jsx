0586cd6728f1356ea1c4fbb60a1b20351308ab44
seyitbugraerden <seyitbugraerden@gmail.com>
2024-04-28 01:54:34 +0300

    Footer

--- components/layouts/Footer.jsx
+++ components/layouts/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = () => {
       <div className="container mx-auto pt-16 pb-6">
         <div className="flex md:justify-between justify-center text-center flex-wrap md:gap-y-0 gap-y-6 ">
           <div className="md:flex-1">
-            <Title addClass="text-[30px]">Contact Us</Title>
+            <Title addClass="text-[30px] ">Contact Us</Title>
             <div className="flex flex-col gap-y-2 mt-3">
-              <div>
+              <div className="hover:text-primary cursor-pointer">
                 <i className="fa fa-map-marker"></i>
                 <span className="inline-block ml-2">Location</span>
               </div>
-              <div>
+              <div className="hover:text-primary cursor-pointer">
                 <i className="fa fa-phone"></i>
                 <span className="inline-block ml-2">Call +01 1234567890</span>
               </div>
-              <div>
+              <div className="hover:text-primary cursor-pointer">
                 <i className="fa fa-envelope"></i>
                 <span className="inline-block ml-2">demo@gmail.com</span>
               </div>
@@ -33,36 +33,36 @@ const Footer = () => {
               It uses a dictionary of over 200 Latin words, combined with
             </p>
             <div className="flex items-center justify-center mt-5 gap-x-2">
-              <a
+              <Link
                 href=""
                 className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:bg-primary hover:text-secondary"
               >
                 <i className="fa fa-facebook "></i>
-              </a>
-              <a
+              </Link>
+              <Link
                 href=""
                 className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:bg-primary hover:text-secondary"
               >
                 <i className="fa fa-twitter "></i>
-              </a>
-              <a
+              </Link>
+              <Link
                 href=""
                 className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:bg-primary hover:text-secondary"
               >
                 <i className="fa fa-linkedin "></i>
-              </a>
-              <a
+              </Link>
+              <Link
                 href=""
                 className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:bg-primary hover:text-secondary"
               >
                 <i className="fa fa-instagram "></i>
-              </a>
-              <a
+              </Link>
+              <Link
                 href=""
                 className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:bg-primary hover:text-secondary"
               >
                 <i className="fa fa-pinterest "></i>
-              </a>
+              </Link>
             </div>
           </div>
           <div className="md:flex-1">
