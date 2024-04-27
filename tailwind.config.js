38f78fa59bd58dc3c5ae8239056a66b0c9c13475
seyitbugraerden <seyitbugraerden@gmail.com>
2024-04-27 03:27:03 +0300

    Header Comp Beginning

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {
-     colors:{
-      primary : '#ffbe33',
-      secondary : '#222831'
-     },
-     fontFamily:{
-      dancing : ["Dancing Script", "cursive"]
-     }
+      conteiner: {
+        screens: {
+          xs: "375px",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1140px",
+          "2xl": "1140px",
+        },
+      },
+      colors: {
+        primary: "#ffbe33",
+        secondary: "#222831",
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
+      },
     },
   },
   plugins: [],
