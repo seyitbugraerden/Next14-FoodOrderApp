6a211b4116c520dc0af36fd22f409892a8ce6f14
seyitbugraerden <seyitbugraerden@gmail.com>
2024-04-28 00:03:52 +0300

    Updates

--- app/page.js
+++ app/page.js
@@ -1,10 +1,3 @@
-import Header from "@/components/layouts/Header";
-import Carousel from "@/components/Carousel";
-
 export default function Home() {
-  return (
-    <main>
-      
-    </main>
-  );
+  return <main></main>;
 }
