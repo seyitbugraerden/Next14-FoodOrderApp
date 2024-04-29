7706a7c94003651bf8c17dbf2eae757349f996f9
seyitbugraerden <seyitbugraerden@gmail.com>
2024-04-29 17:07:44 +0300

    Menu Wrapper

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -1,29 +1,63 @@
 import Image from "next/legacy/image";
 import Link from "next/link";
 import { RiShoppingCart2Fill } from "react-icons/ri";
-const MenuItem = ({ products }) => {
+const MenuItem = ({ products, selected }) => {
   return (
-    <div className="bg-secondary rounded-3xl">
-      <div className="w-full  bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
-        <div className="relative w-36 h-36 hover:scale-110 transition-all">
-          <Image src={products.img} alt="" layout="fill" objectFit="contain" />
+    <>
+      {selected !== "" &&
+        (products.title.includes(selected) ? (
+          <div className="bg-secondary rounded-3xl my-2 zoom-scale">
+            <div className="w-full bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
+              <div className="relative w-36 h-36 hover:scale-110 transition-all">
+                <Image
+                  src={products.img}
+                  alt=""
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            </div>
+            <div className="p-[25px] text-white">
+              <h4 className="text-xl font-semibold">{products.title}</h4>
+              <p className="text-[15px] text-justify">{products.desc}</p>
+              <div className="flex justify-between items-center mt-4">
+                <span>${products.price}</span>
+                <Link href="/product/12">
+                  <button className="btn-primary !w-10 !h-10 !rounded-full !p-0 grid place-content-center">
+                    <RiShoppingCart2Fill />
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ) : null)}
+      {selected === "All" && (
+        <div className="bg-secondary rounded-3xl my-2 zoom-scale">
+          <div className="w-full bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
+            <div className="relative w-36 h-36 hover:scale-110 transition-all">
+              <Image
+                src={products.img}
+                alt=""
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+          </div>
+          <div className="p-[25px] text-white">
+            <h4 className="text-xl font-semibold">{products.title}</h4>
+            <p className="text-[15px] text-justify">{products.desc}</p>
+            <div className="flex justify-between items-center mt-4">
+              <span>${products.price}</span>
+              <Link href="/product/12">
+                <button className="btn-primary !w-10 !h-10 !rounded-full !p-0 grid place-content-center">
+                  <RiShoppingCart2Fill />
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="p-[25px] text-white">
-        <h4 className="text-xl font-semibold">{products.title}</h4>
-        <p className="text-[15px] text-justify">
-          {products.desc}
-        </p>
-        <div className="flex justify-between items-center mt-4">
-          <span>${products.price}</span>
-          <Link href="/product/12">
-            <button className="btn-primary !w-10 !h-10 !rounded-full !p-0 grid place-content-center">
-              <RiShoppingCart2Fill />
-            </button>
-          </Link>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -47,7 +47,7 @@ const MenuWrapper = () => {
       desc: "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
       price: 10,
     },
-    
+
     {
       key: 6,
       img: "/images/f1.png",
@@ -70,28 +70,57 @@ const MenuWrapper = () => {
       price: 10,
     },
   ]);
+  const [selectedFilter, setSelectedFilter] = useState("All");
   return (
     <div className="container mx-auto  mb-16">
       <div className="flex flex-col items-center w-full">
         <Title addClass="text-[40px]">Our Menu</Title>
         <div className="mt-10">
-          <button className="px-6 py-2 bg-secondary rounded-3xl text-white">
+          <button
+            onClick={() => {
+              setSelectedFilter("All");
+            }}
+            className={`px-6 py-2 rounded-3xl text-secondary hover:bg-secondary hover:text-white ${
+              selectedFilter === "All" ? "bg-secondary text-white" : ""
+            }`}
+          >
             All
           </button>
-          <button className="px-6 py-2 rounded-3xl hover:bg-secondary hover:text-white transition-all">
+          <button
+            onClick={() => {
+              setSelectedFilter("Burger");
+            }}
+            className={`px-6 py-2 rounded-3xl hover:bg-secondary hover:text-white transition-all ${
+              selectedFilter === "Burger" ? "bg-secondary text-white" : ""
+            }`}
+          >
             Burger
           </button>
-          <button className="px-6 py-2  rounded-3xl hover:bg-secondary hover:text-white transition-all">
+          <button
+            onClick={() => {
+              setSelectedFilter("Pizza");
+            }}
+            className={`px-6 py-2 rounded-3xl hover:bg-secondary hover:text-white transition-all ${
+              selectedFilter === "Pizza" ? "bg-secondary text-white" : ""
+            }`}
+          >
             Pizza
           </button>
-          <button className="px-6 py-2  rounded-3xl hover:bg-secondary hover:text-white transition-all">
-            Drink
+          <button
+            onClick={() => {
+              setSelectedFilter("Fries");
+            }}
+            className={`px-6 py-2 rounded-3xl hover:bg-secondary hover:text-white transition-all ${
+              selectedFilter === "Fries" ? "bg-secondary text-white" : ""
+            }`}
+          >
+            Fries
           </button>
         </div>
       </div>
       <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
         {items.map((item, index) => (
-          <MenuItem key={index} products={item} />
+          <MenuItem key={index} products={item} selected={selectedFilter} />
         ))}
       </div>
     </div>
